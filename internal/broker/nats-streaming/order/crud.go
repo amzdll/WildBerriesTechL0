096bd57e5be3c04f59a473de558359576ac74f0f
glenpoin <glenpoin@student.21-school.ru>
2024-06-16 06:08:32 +0300

    feat(internal/broker): cache recovery, processing of messages received while the service was unavailable

--- internal/broker/nats-streaming/order/crud.go
+++ internal/broker/nats-streaming/order/crud.go
@@ -3,23 +3,41 @@ package order
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/nats-io/stan.go"
-	"log"
 	"wb/internal/broker/nats-streaming/order/message"
+	"wb/internal/db/pg"
 )
 
-func (h MessageHandler) Create(m *stan.Msg) {
+func (h MessageHandler) create(m *stan.Msg) {
 	var order message.Order
 	if err := json.Unmarshal(m.Data, &order); err != nil {
+		h.logger.Error("Can't unmarshall data", err)
 		return
 	}
 	if err := h.service.Create(context.Background(), order.ToModel()); err != nil {
-		log.Println(order.OrderUID)
 		h.logger.Error("Data processing error", err)
 		return
 	}
 
 	if err := m.Ack(); err != nil {
+		h.logger.Error("Data processing confirmation error", err)
+	}
+}
+
+func (h MessageHandler) processPending(m *stan.Msg) {
+	var order message.Order
+	if err := json.Unmarshal(m.Data, &order); err != nil {
+		h.logger.Error("Can't unmarshall data", err)
+		return
+	}
+	err := h.service.Create(context.Background(), order.ToModel())
+	if err != nil && !errors.Is(err, pg.ErrDuplicateKey) {
+		h.logger.Error("Data processing error", err)
+		return
+	}
+
+	if err = m.Ack(); err != nil {
 		h.logger.Error("Data processing confirmation error", err)
 	}
 	h.logger.Info("Order created")
--- internal/broker/nats-streaming/order/handler.go
+++ internal/broker/nats-streaming/order/handler.go
@@ -24,12 +24,33 @@ func New(service Service, logger *logger.Logger) *MessageHandler {
 	}
 }
 
+func (h MessageHandler) handlePendingMessages(conn stan.Conn) {
+	sub, err := conn.Subscribe(
+		"orders",
+		h.processPending,
+		stan.DeliverAllAvailable(),
+		stan.AckWait(30*time.Second),
+		stan.SetManualAckMode(),
+	)
+	if err != nil {
+		h.logger.Fatal("Error when unsubscribing to the channel", err)
+	}
+
+	defer func(sub stan.Subscription) {
+		err = sub.Unsubscribe()
+		if err != nil {
+			h.logger.Fatal("Error when connecting to a channel", err)
+		}
+	}(sub)
+}
+
 func (h MessageHandler) Subscribe(conn stan.Conn) error {
 	var err error
+	h.handlePendingMessages(conn)
 	if _, err = conn.Subscribe(
 		"orders",
-		h.Create,
-		stan.DeliverAllAvailable(),
+		h.create,
+		stan.StartAtTime(time.Now().Add(time.Second*5)),
 		stan.AckWait(30*time.Second),
 		stan.SetManualAckMode(),
 	); err != nil {
